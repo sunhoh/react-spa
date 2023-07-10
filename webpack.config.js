b8c895d10d20cf4a8215e7f6f75ab1f72c1a86cd
sunhoh <dltjsgho94@gmail.com>
2023-07-10 11:40:59 +0900

    add: add a loader for styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const publicDir = path.resolve(__dirname, "./public");
 
 module.exports = {
@@ -20,6 +21,10 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
@@ -27,5 +32,6 @@ module.exports = {
       template: publicDir + "/index.html",
       filename: "./index.html",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
